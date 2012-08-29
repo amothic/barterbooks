bc36a611e906af0c8128237c9d24172f8bd0c340
Daichi TOMA <amothic@gmail.com>
2012-08-29 23:01:59 +0900

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Sachie Shinzato"
+    email    "e125723@ie.u-ryukyu.ac.jp"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
