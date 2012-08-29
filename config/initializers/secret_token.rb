c9f314a288e80a3f86d0c05d9487ea3fc6935308
Daichi TOMA <amothic@gmail.com>
2012-08-29 16:28:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barterbooks::Application.config.secret_token = 'ad9a5b0abd1f35ce40897f1c7689db7d8e2471226bd13608312341d07768f284a1e0e277d1fe1b6e224f0c5823655663bd0c70c8dfc1cc64dfe4b68d0142723f'
