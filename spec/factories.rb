c8377818dc0130c79a437055d921efc2fe2b10cd
Daichi TOMA <amothic@gmail.com>
2012-08-30 01:09:44 +0900

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Sachie Shinzato"
-    email    "e125723@ie.u-ryukyu.ac.jp"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@ie.u-ryukyu.ac.jp"}   
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
